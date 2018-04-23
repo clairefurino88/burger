04f34a52ee247965ab426e3cc7bea1b21081c013
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-04-23 16:54:56 -0400

    fixed config

--- config/connections.js
+++ config/connections.js
@@ -1,23 +1,6 @@
 
 var mysql = require("mysql");
 
-var PORT = process.env.PORT || 3000;
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3307,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-  
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 //exporting to orm.js
 module.exports = connection;
