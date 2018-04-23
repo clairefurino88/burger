90e35cf8f3657c02d613c7ea42a8c6537a2b4dd9
clairefurino88 <33135335+clairefurino88@users.noreply.github.com>
2018-04-23 18:27:06 -0400

    fixed spacing

--- config/connections.js
+++ config/connections.js
@@ -5,9 +5,9 @@ var connection;
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } 
+
 else{
   connection = mysql.createConnection({
-
   host: "localhost",
   user: "root",
   password: "root",
